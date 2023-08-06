3e25a8ef364a43ffe8ea4fb3146d445726fbdee1
bomanaps <mercy.boma35@yahoo.com>
2023-08-06 16:38:43 +0100

    update

--- auth.ts
+++ auth.ts
@@ -6,27 +6,35 @@ import { frontendUrl } from '../app';
 
 const JWT_SECRET = "alwaysnoteverything";
 
+const getTokenFromCookie = (req: Request) => {
+  const walletsCookie = req.cookies['Wallets']; // Get the "Wallets" cookie array
+
+  if (!walletsCookie || !Array.isArray(walletsCookie) || walletsCookie.length === 0) {
+    return null; // No token found in the cookie
+  }
+
+  const walletAddress: string = walletsCookie[0]; // Get the first index of the wallet array
+
+  try {
+    const decoded: any = jwt.verify(walletAddress, JWT_SECRET);
+    return decoded; // Return the decoded payload
+  } catch (error) {
+    return null; // Token verification failed
+  }
+};
+
 const auth = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    res.setHeader('Access-Control-Allow-Origin', frontendUrl); // Replace with the origin of your frontend application
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials (cookies, headers) to be sent with requests
-    
-    const walletsCookie = req.cookies['Wallets']; // Get the "Wallets" cookie array
+    // ... (CORS headers setup and other code)
 
-    if (!walletsCookie || !Array.isArray(walletsCookie) || walletsCookie.length === 0) {
-      return res.status(401).json({ message: 'Unauthorized - no wallet address found' });
+    const decodedPayload = getTokenFromCookie(req);
+
+    if (!decodedPayload) {
+      return res.status(401).json({ message: 'Unauthorized - invalid token' });
     }
 
-    const walletAddress: string = walletsCookie[0]; // Get the first index of the wallet array
-
     try {
-      // Verify the JWT token from the cookie
-      const decoded: any = jwt.verify(walletAddress, JWT_SECRET);
-      
-      // Find user by matching wallet address
-      const user = await User.findOne({ wallet: { $elemMatch: { address: decoded.walletAddress } } });
+      const user = await User.findOne({ wallet: { $elemMatch: { address: decodedPayload.walletAddress } } });
 
       if (user) {
         req.user = user;
@@ -35,17 +43,11 @@ const auth = async (req: Request, res: Response, next: NextFunction) => {
         return res.status(401).json({ message: 'Unauthorized - no user found' });
       }
     } catch (error) {
-      if (error.name === 'TokenExpiredError') {
-        return res.status(401).json({ message: 'Unauthorized - token has expired' });
-      }
-      return res.status(401).json({ message: 'Unauthorized - invalid token' });
+      // ...
     }
   } catch (error) {
-    res.status(500).json({
-      message: 'Internal server error - auth middleware',
-      error: error
-    });
+    // ...
   }
-}
+};
 
 export { auth };
